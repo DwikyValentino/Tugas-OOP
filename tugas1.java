115328310ae6aca972cad8d3de6cfedd3035b421
Dwiky Valentino <48315646+DwikyValentino@users.noreply.github.com>
2019-03-08 10:21:46 +0700

    Create tugas1.java

---
+++ tugas1.java
@@ -0,0 +1,8 @@
+public class BDwiky {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Dwiky Valentino");
+        System.out.println("NIM     : 17090053");
+        System.out.println("Kelas   : 4B");
+    }
+    
+}
